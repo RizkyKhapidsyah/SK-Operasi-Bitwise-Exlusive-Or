e3e074f976d0a1338298d0b6502ae459489910af
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-01-01 08:23:24 +0700

    Add project files.

---
+++ SK-Operasi-Bitwise-Exlusive-Or/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+/* Rizky Khapidsyah */
+
+int main() {
+	int a, x, y;
+	
+	cout << "Masukan Nilai X = ";
+	cin >> x;
+	cout << "Masukan Nilai Y = ";
+	cin >> y;
+	
+	a = x ^ y;
+	
+	cout << "\n";
+	cout << "Hasil dari " << x << " ^ " << y << " = " << a << endl;
+	
+
+	_getch();
+	return 0;
+}
